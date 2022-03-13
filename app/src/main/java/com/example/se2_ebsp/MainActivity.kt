05dbd6d44dbac2fa954acd96ca1e98bb3e9c0e3f
Wolfgang Prett <wolfgang.prett@topinform.info>
2022-03-13 23:58:42 +0100

    tried to implement functions but failed due to no idea how coroutines in kotlin actually work

--- app/src/main/java/com/example/se2_ebsp/MainActivity.kt
+++ app/src/main/java/com/example/se2_ebsp/MainActivity.kt
@@ -2,10 +2,74 @@ package com.example.se2_ebsp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import java.net.Socket
+import kotlinx.coroutines.*
+import org.w3c.dom.Text
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sendRequestBtn = findViewById<Button>(R.id.button_getResponse)
+        sendRequestBtn.setOnClickListener {
+                   onClickSend()
+        }
+        val calculateButton = findViewById<Button>(R.id.button_calculate)
+        sendRequestBtn.setOnClickListener {
+                calculate()
+        }
+
     }
+
+
+    fun calculate(){
+        val matrNrString = this.getMatrikelNr()
+
+        val matrNrInt = Integer.parseInt(matrNrString)
+        var index = 0
+        var sum = 0;
+        while(index < matrNrString.length){
+            sum += Integer.parseInt(matrNrString.get(index).toString())
+            index++
+        }
+
+        val textView : TextView = findViewById(R.id.text_response)
+        textView.text = Integer.toBinaryString(sum);
+
+    }
+
+     fun onClickSend() = runBlocking {
+
+
+            createConnection()
+
+    }
+
+    fun getMatrikelNr():String{
+          val matNr: EditText = findViewById(R.id.field_number)
+        return matNr.text.toString()
+    }
+
+
+
+
+    suspend fun createConnection() = coroutineScope {
+
+        val job = launch {
+            val socket = Socket("se2-isys.aau.at",53212)
+            socket.isConnected
+            delay(100L)
+        }
+        job.join()
+
+    }
+
+     fun writeSomething(something:String) {
+         val textView = findViewById<TextView>(R.id.text_response)
+         textView.text = something
+     }
 }
