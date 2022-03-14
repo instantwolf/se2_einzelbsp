55263bd24f0a84dcd3d81a69126f9a529abe16fd
Wolfgang Prett <wolfgang.prett@topinform.info>
2022-03-14 14:01:14 +0100

    removed another unused local variable within the calculate method, cause we actually use writeSomething() method to fetch corresponding output fields and set the text.

--- app/src/main/java/com/example/se2_ebsp/MainActivity.kt
+++ app/src/main/java/com/example/se2_ebsp/MainActivity.kt
@@ -35,7 +35,6 @@ class MainActivity : AppCompatActivity()  {
             index++
         }
 
-        val textView : TextView = findViewById(R.id.text_response)
        writeSomething(Integer.toBinaryString(sum))
 
     }
